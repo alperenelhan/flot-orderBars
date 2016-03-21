d6dec1bc0f6ba06d25aa117372bcda18a6b6be59
Alperen Elhan <alperen@elhan.org>
2016-03-21 21:01:21 +0200

    add desc

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'alperen:flot-orderbars',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'OrderBars plugin for flot:flot',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/alperenelhan/flot-orderBars.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -12,7 +12,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
-  api.use('flot', 'client');
-  api.imply('flot', 'client');
+  api.use('flot:flot', 'client');
+  api.imply('flot:flot', 'client');
   api.addFiles('flot-orderbars.js', 'client');
 });
